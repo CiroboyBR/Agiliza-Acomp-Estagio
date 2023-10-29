2756c17492114b126df4232fae45a4abfaaf5079
Ciro <CiroboyBR@users.noreply.github.com>
2023-10-29 13:32:02 -0300

    Create codigos.js

---
+++ codigos.js
@@ -0,0 +1,32 @@
+//Preenche automaticamente alguns campos do formulário paga agilizar as tarefas repetitivas
+
+document.getElementById("obsAP").value = "SEM OBSERVAÇÕES"
+document.getElementById("obsAA").value = "SEM OBSERVAÇÕES"
+
+document.getElementById("criterio:0:j_idt286:0").checked = true
+document.getElementById("criterio:1:j_idt286:0").checked = true
+document.getElementById("criterio:2:j_idt286:0").checked = true
+document.getElementById("criterio:3:j_idt286:0").checked = true
+document.getElementById("criterio:4:j_idt286:0").checked = true
+document.getElementById("criterio:5:j_idt286:0").checked = true
+document.getElementById("criterio:6:j_idt286:0").checked = true
+document.getElementById("criterio:7:j_idt286:0").checked = true
+document.getElementById("criterio:8:j_idt286:0").checked = true
+document.getElementById("criterio:9:j_idt286:0").checked = true
+document.getElementById("criterio:10:j_idt286:0").checked = true
+document.getElementById("criterio:11:j_idt286:0").checked = true
+document.getElementById("criterio:12:j_idt286:0").checked = true
+document.getElementById("criterio:13:j_idt286:0").checked = true
+document.getElementById("criterio:14:j_idt286:0").checked = true
+document.getElementById("criterio:15:j_idt286:0").checked = true
+document.getElementById("criterio:16:j_idt286:0").checked = true
+document.getElementById("criterio:17:j_idt286:0").checked = true
+document.getElementById("criterio:18:j_idt286:0").checked = true
+document.getElementById("criterio:19:j_idt286:0").checked = true
+document.getElementById("criterio:20:j_idt286:0").checked = true
+document.getElementById("criterio:21:j_idt286:0").checked = true
+document.getElementById("criterio:22:j_idt286:0").checked = true
+
+//ROLA PRO FINAL DA PAGINA
+const scrollingElement = (document.scrollingElement || document.body);
+scrollingElement.scrollTop = scrollingElement.scrollHeight;
